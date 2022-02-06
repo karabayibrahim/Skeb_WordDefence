cb04170e4916f2f8c06fd0d917904f9be1b3a58e
İbrahim karabay <46526531+karabayibrahim@users.noreply.github.com>
2022-02-07 02:15:55 +0300

    Last

---
+++ SkebWordDefence/Assets/Game/Scripts/NpcController/NpcController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcController : MonoBehaviour
+{
+    private float _speed = 5f;
+
+
+    public float Speed
+    {
+        get
+        {
+            return _speed;
+        }
+        set
+        {
+            if (Speed == value)
+            {
+                return;
+            }
+            _speed = value;
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveSystem();
+    }
+
+    private void MoveSystem()
+    {
+        transform.Translate(0, 0, _speed * Time.deltaTime);
+    }
+}
---
+++ SkebWordDefence/Assets/Game/Scripts/SpawnManager/SpawnManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SpawnManagerScriptableObject", order = 1)]
+public class SpawnManager : ScriptableObject
+{
+    public GameObject SpawnObject;
+    
+
+
+    private void SpawnObjectMethod()
+    {
+
+    }
+}
